6fcd62196c194e5aada1b462f2afcde376921190
Валерий Войтюк <polarwolfdshkshk@gmail.com>
2025-09-17 03:28:14 +0500

    Task2

---
+++ Tyuiu.VoityukVT.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VoityukVT.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.VoityukVT.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMassageValid()
+        {
+          
+            var name = "Валерий";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Валерий", res);
+        }
+    }
+}
